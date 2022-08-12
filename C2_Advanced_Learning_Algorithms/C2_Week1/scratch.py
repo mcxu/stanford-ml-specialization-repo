663ec358b203342359881202d7f8eb6a857a7702
mcxu <michaelxu79@gmail.com>
2022-08-12 00:05:51 -0400

    Adding files for Course 2 Week 2 Neural Networks with Tensorflow

--- C2_Advanced_Learning_Algorithms/C2_Week1/scratch.py
+++ C2_Advanced_Learning_Algorithms/C2_Week1/scratch.py
@@ -13,3 +13,20 @@ print("w at j={j}: ", w)
 
 # ================= Matrix Multiplication Rules =================
 
+A = np.array([
+    [200],
+    [17]
+])
+
+b = np.array([[-1, 1, 2]])
+
+test1 = A * W
+print("test1:\n", test1)
+
+test2 = np.matmul(A.T, W)
+print("test2:\n", test2)
+
+test3 = A.T @ W
+print("test3:\n", test3)
+
+assert np.array_equal(test2, test3)
